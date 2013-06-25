fe5e4420142449f8efa4fd9c94ddafeeed068c49
Kbjohnson <kylejson@mail.com>
2013-06-25 17:00:34 -0400

    added files to pub in master

---
+++ pub/index.php
@@ -0,0 +1,114 @@
+<?php
+
+?>
+
+<html>
+	<head>
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap-responsive.min.css">
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	
+	<body class="container">
+		<!--Start Menu-->
+		<div class="wrapper start-menu">
+			<div class="row">
+				
+				<div class="sidebar span2">
+					<ul>
+						<li><img src=""></li>
+						<li><img src=""></li>
+						<li><img src=""></li>
+						<li><img src=""></li>
+						<li><img src=""></li>
+					</ul>
+				</div>
+				
+				<div class="main span7 offset1">
+					<div class="page-header">
+					  <h2>Guess That Song <small>powered by Grooveshark</small></h2>
+					</div>	
+					
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<button class='btn btn-large btn-warning pull-right'><i class="icon-music"></i> PLAY</button>
+				</div>
+			</div>
+		</div>
+
+		<!--Game State-->
+		<div class="wrapper game-state">
+			<div class="row">
+				
+				<div class="sidebar span2">
+					<ul>
+						<li><img src=""></li>
+						<li><img src=""></li>
+						<li class="active"><img src=""></li>
+					</ul>
+				</div>
+				
+				<div class="main span7 offset1">
+					<div class="page-header">
+					  <h2>Guess That Song <small>powered by Grooveshark</small></h2>
+					</div>
+					
+					<div class="row">
+						<form class="span6">	
+							<p>Guess!</p>
+							 <input type="text" class="input-xlarge search-query" placeholder="Guess that song here!">
+						</form>
+
+						<div>
+							<p>Timer: 15</p>
+						</div>
+					</div>
+					
+					<div class="row">
+						<div class="span6">
+							<p>Song 6 of 12</p>
+							<div class="progress progress-warning progress-striped active">
+							  <div class="bar" style="width:50%"></div>
+							</div>
+						</div>
+						
+						<div>
+							<p>Points: 100</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		
+		<!--Win Screen-->
+		<div class="wrapper win-screen">
+			<div class="row">
+				
+				<div class="sidebar span2">
+					<ul>
+						<li><img src=""></li>
+						<li><img src=""></li>
+						<li><img src=""></li>
+					</ul>
+				</div>
+				
+				<div class="main span7 offset1">
+					<div class="page-header">
+					  <h2>Guess That Song <small>powered by Grooveshark</small></h2>
+					</div>
+
+					<div class="row">
+						<div class="span6">	
+							<p>You Scored: 1200 pts!</p>
+							<p>&larr; Share!</p>
+						</div>
+						<button class='btn btn-large btn-warning pull-right'></i> PLAY AGAIN</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+	<script type="text/javascript" src=""></script>	
+	</body>
+
+</html>
