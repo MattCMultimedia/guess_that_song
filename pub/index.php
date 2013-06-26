f672092d6e6b2d4c65aa252e2dd706fa4fd4b64c
Kbjohnson <kylejson@mail.com>
2013-06-25 20:21:16 -0400

    edits to index.php design

--- pub/index.php
+++ pub/index.php
@@ -11,11 +11,12 @@
 	</head>
 	
 	<body class="container">
-		<!--Start Menu-->
+		<!--{*Start Menu*}-->
 		<div class="wrapper start-menu">
 			<div class="row">
 				
 				<div class="sidebar span2">
+					<!--{*This ul should have as many <li>'s as genres*}-->
 					<ul>
 						<li><img src=""></li>
 						<li><img src=""></li>
@@ -29,18 +30,30 @@
 					<div class="page-header">
 					  <h2>Guess That Song <small>powered by Grooveshark</small></h2>
 					</div>	
+					<!--{*a form goes here if instead of the <p> they are not loggined into grooveshark*}-->
+					<!--{*
+						<form action="" method="POST">
+							<h4>Username:</h4>
+							<input class="input" type="text" name="username" >
+							<h4>Password:</h4>
+							<input class="input" type="password" name="password" >
+						</form>
+						*}-->
 					
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					
+					<!--{*This button should say Login to GS if they are not logged into grooveshark*}-->
 					<button class='btn btn-large btn-warning pull-right'><i class="icon-music"></i> PLAY</button>
 				</div>
 			</div>
 		</div>
 
-		<!--Game State-->
+		<!--{*Game State*}-->
 		<div class="wrapper game-state">
 			<div class="row">
 				
 				<div class="sidebar span2">
+					<!--{*This will be filled with <li>'s corresponding ' *}-->
 					<ul>
 						<li><img src=""></li>
 						<li><img src=""></li>
@@ -54,33 +67,33 @@
 					</div>
 					
 					<div class="row">
-						<form class="span6">	
-							<p>Guess!</p>
-							 <input type="text" class="input-xlarge search-query" placeholder="Guess that song here!">
+						<form class="span3">	
+							<h4>Guess!</h4>
+							 <input type="text" class="search-query" >
 						</form>
-
-						<div>
-							<p>Timer: 15</p>
-						</div>
-					</div>
-					
-					<div class="row">
-						<div class="span6">
-							<p>Song 6 of 12</p>
+						
+						<div class="span4">
+							<h4>Song <span class="current_song">6</span> of <span class="total_songs">12</span></h4>
 							<div class="progress progress-warning progress-striped active">
 							  <div class="bar" style="width:50%"></div>
 							</div>
 						</div>
 						
-						<div>
-							<p>Points: 100</p>
+					</div>
+					
+					<div class="row">
+						
+						<div class="span4 offset3">
+							<h4>Timer: 12</h4>
+							<h4>Points: 100</h4>
 						</div>
+					
 					</div>
 				</div>
 			</div>
 		</div>
 		
-		<!--Win Screen-->
+		<!--{*Win Screen*}-->
 		<div class="wrapper win-screen">
 			<div class="row">
 				
@@ -99,8 +112,8 @@
 
 					<div class="row">
 						<div class="span6">	
-							<p>You Scored: 1200 pts!</p>
-							<p>&larr; Share!</p>
+							<h4>You Scored: 1200 pts!</h4>
+							<h4>&larr; Share!</h4>
 						</div>
 						<button class='btn btn-large btn-warning pull-right'></i> PLAY AGAIN</button>
 					</div>
